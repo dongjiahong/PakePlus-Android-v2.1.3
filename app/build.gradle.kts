4b58bf4ed7bd1e5ef40c6f68e22706fa62f10d0f
jiahong.dong <dongjiahong@hotmail.com>
2025-11-24 14:47:49 +0800

    build: 打包 aistudio v0.0.4
    - URL: https://aistudio.google.com/apps
    - 包名: com.aistudio.djh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "www.djh.aistudio.com"
+        applicationId = "com.aistudio.djh"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
